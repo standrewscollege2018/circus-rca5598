c130922f27fc45085cbab9e690c72547badcd0f7
rca5598 <42048590+rca5598@users.noreply.github.com>
2018-08-27 09:51:00 +1200

    Initial Tkinter Formatting
    Setting up class/ Tkinter Mainloop etc

---
+++ Comic Assessment.py
@@ -0,0 +1,33 @@
+from tkinter import *
+root = Tk()
+root.title("Comic Assessment")
+root.geometry('500x400')
+class Ticket:
+
+     
+    def __init__(self, time, capacity, cost):
+        self._time = time
+        self._capacity = capacity
+        self._cost = cost
+        tickets.append(self)
+        pizza_names.append(name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+root.mainloop() 
